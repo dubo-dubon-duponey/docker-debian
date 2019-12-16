47fd09fd62c37593286e73ce8ed1ef8735689495
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-12-16 11:33:42 -0800

    Tweak date to be simpler / uniform accross all images
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 
 # The suite and snapshot "date" from which you want to build your Debian buster image.
 DEBIAN_SUITE=${DEBIAN_SUITE:-buster}
-DEBIAN_DATE=${DEBIAN_DATE:-2019-12-01T00:00:00Z}
+DEBIAN_DATE=${DEBIAN_DATE:-2019-12-01}T00:00:00Z
 
 # The destination/name to use when pushing your Debian image, and the platforms you target
 IMAGE_NAME="${IMAGE_NAME:-docker.io/dubodubonduponey/debian}"
