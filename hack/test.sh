2e26aa9b1b369b88c17bb8763c1d46d7b4542065
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2024-03-17 15:51:09 -0700

    CI, episode 367

--- hack/test.sh
+++ hack/test.sh
@@ -4,11 +4,9 @@ set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 root="$(cd "$(dirname "${BASH_SOURCE[0]}")" 2>/dev/null 1>&2 && pwd)/../"
 readonly root
 
-# Requires a buildkit host and a cue binary
 if ! "$root/hack/build.sh" \
-    --inject registry="docker.io/dubodubonduponey" \
     --inject progress=plain \
-	  --inject date=2021-08-01 \
+	  --inject date=2024-03-01 \
 	  --inject suite=bookworm \
     --inject platforms=linux/amd64,linux/arm64 \
   	debootstrap "$@"; then
@@ -21,7 +19,7 @@ result1="$(cat "$root"/context/cache/**/*.sha)"
 if ! "$root/hack/build.sh" \
     --inject registry="" \
     --inject progress=plain \
-	  --inject date=2021-08-01 \
+	  --inject date=2024-03-01 \
 	  --inject suite=bookworm \
     --inject platforms=linux/amd64,linux/arm64 \
   	debootstrap "$@"; then
